c72de7bfcad4c500f407dc2bf7d7b4661e3d7fd5
osvaldorj <os7rj89@gmail.com>
2016-07-01 16:54:21 -0700

    Location
    Location

---
+++ app/src/main/java/com/example/os/simplelocationcomparison/MainActivity.java
@@ -0,0 +1,233 @@
+package com.example.os.simplelocationcomparison;
+
+/**** Some definitions *****/
+/*
+    GPS_PROVIDER: The most accurate method. Uses the built-in GPS receiver.
+    NETWORK_PROVIDER: Uses data collected from the surrounding cell towers and WiFi access point.
+    PASSIVE_PROVIDER: Indicates that your app doesn't want to initiate a location fix.
+                      It uses the location updates received by other applications/services.
+                      In other words can use GPS or NETWORK providers.
+ */
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
+    TextView longitudeValueBest, latitudeValueBest;
+    TextView longitudeValueGPS, latitudeValueGPS;
+    TextView longitudeValueNetwork, latitudeValueNetwork;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        longitudeValueBest = (TextView)findViewById(R.id.longitudeValueBest);
+        latitudeValueBest = (TextView)findViewById(R.id.latitudeValueBest);
+        longitudeValueGPS = (TextView)findViewById(R.id.longitudeValueGPS);
+        latitudeValueGPS = (TextView)findViewById(R.id.latitudeValueGPS);
+        longitudeValueNetwork = (TextView)findViewById(R.id.longitudeValueNetwork);
+        latitudeValueNetwork = (TextView)findViewById(R.id.latitudeValueNetwork);
+    }
+
+    private boolean checkLocation(){
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private void showAlert(){
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location").setMessage("Your Location Settings is set to 'Off'. \nPlease Enable Location to "+
+        "use this app").setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(myIntent);
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled(){
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public void toggleGPSUpdates(View view) {
+        if(!checkLocation())
+            return;
+        Button button = (Button) view;
+        if(button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerGPS);
+            button.setText(R.string.resume);
+        }
+        else {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 2 * 10 * 1000, 10, locationListenerGPS);
+            button.setText(R.string.pause);
+        }
+    }
+
+    public void toggleBestUpdates(View view) {
+        if(!checkLocation())
+            return;
+        Button button = (Button) view;
+        if(button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerBest);
+            button.setText(R.string.resume);
+        }
+        else {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            String provider = locationManager.getBestProvider(criteria, true);
+            if(provider != null) {
+                locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
+                button.setText(R.string.pause);
+                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void toggleNetworkUpdates(View view){
+        if(!checkLocation())
+            return;
+        Button button = (Button) view;
+        if(button.getText().equals(getResources().getString(R.string.pause))){
+            locationManager.removeUpdates(locationListenerNetwork);
+            button.setText(R.string.resume);
+        }else{
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 60*1000,10,locationListenerNetwork);
+            Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();;
+            button.setText(R.string.pause);
+
+        }
+    }
+
+    private final LocationListener locationListenerBest = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            longitudeBest = location.getLongitude();
+            latitudeBest = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueBest.setText(longitudeBest + "");
+                    latitudeValueBest.setText(latitudeBest + "");
+                    Toast.makeText(MainActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueNetwork.setText(longitudeNetwork + "");
+                    latitudeValueNetwork.setText(latitudeNetwork + "");
+                    Toast.makeText(MainActivity.this, "Network Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueGPS.setText(longitudeGPS + "");
+                    latitudeValueGPS.setText(latitudeGPS + "");
+                    Toast.makeText(MainActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+}
